9c261d948e3f24e83cfbb55121411c02348f88e3
xiphiasnonus <foster.pianist@gmail.com>
2021-04-26 20:18:19 -0400

    update ascii example and screenshots

--- examples/ascii.c
+++ examples/ascii.c
@@ -6,16 +6,25 @@
 
 int main()
 {
-    textmode(C80);
-    setscreensize(WIDTH, HEIGHT);
+    textmode(C40);
+    setscreensize(WIDTH + 1, HEIGHT + 1);
     setscale(3);
     setcursor(CURSOR_NONE);
     setname("ascii.c");
 
     initdos();
 
+    textcolor(WHITE);
+    for ( int i = 0; i < 16; i++ ) {
+        gotoxy(i + 2, 1);
+        cprintf("%X", i);
+        gotoxy(1, i + 2);
+        cprintf("%X", i);
+    }
+    
+    textcolor(LIGHTGRAY);
     for ( int ch = 0; ch <= 255; ch++ ) {
-        gotoxy((ch % WIDTH) + 1, ch / WIDTH + 1);
+        gotoxy((ch % WIDTH) + 2, ch / WIDTH + 2);
         putch(ch);
     }
         
